6f4a0e736b6349e69ed1fe1e7eee76641236bb49
Daniel Garcia <danielrichardgarcia2@gmail.com>
2021-09-26 14:06:37 -0700

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/dashboard/Dashboard.page";
 
 import { Entry } from "./pages/entry/Entry.page";
 
@@ -8,7 +9,7 @@ function App() {
   return <div className="App">
     {/*<Entry />*/}
     <DefaultLayout>
-      //Dashboard
+      <Dashboard />
     </DefaultLayout>
     </div>;
 }
